44e72a99d72e32ba58a113dcc025380fa4a0df7f
Darong Li <dli97@ucsc.edu>
2022-04-19 01:50:54 -0700

    comment

--- src/main.js
+++ src/main.js
@@ -17,4 +17,16 @@ let borderPadding = borderUISize / 3;
 // reserve keyboard variables
 let keyOne, keyR, keyLEFT, keyRIGHT, keyA, keyD, keyJ;
 
-//let gameTimer;
+/*
+Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) : Rocket->Arrow, Spaceships->dragons, explosion->new
+Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20): new dragon that moves faster -> purple dragon
+Replace the UI borders with new artwork (10): new background
+Implement a simultaneous two-player mode (30)
+
+Total: 100
+
+Citation:
+Jiatao Xu, from SJSU, he helps me with the artwork.
+Calex Raffield, same class, he helps me to implement a new timer
+*/
